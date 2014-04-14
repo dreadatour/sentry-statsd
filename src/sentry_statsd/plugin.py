e0cce2bf2740b3a7b4a446c7b4a298ceb7097223
Jonathan Cua <jcua@eventbrite.com>
2014-04-14 13:00:20 -0700

    track_only_new is set to false all the time even though you have the option to toggle it in the form

--- src/sentry_statsd/plugin.py
+++ src/sentry_statsd/plugin.py
@@ -46,7 +46,7 @@ class StatsdPlugin(Plugin):
         port = self.get_option('port', group.project)
         prefix = self.get_option('prefix', group.project)
         add_loggers = self.get_option('add_loggers', group.project)
-        track_only_new = self.get_option('track_only_new', False)
+        track_only_new = self.get_option('track_only_new', group.project)
 
         metric = []
         metric.append(group.project.slug.replace('-', '_'))
